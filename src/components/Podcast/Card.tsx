d96b010185f853a0b23957a6da86b6ff3ca7a4f4
Oscar <ovenor11@proton.me>
2023-02-21 11:56:09 +0100

    fix - content

--- src/components/Podcast/Card.tsx
+++ src/components/Podcast/Card.tsx
@@ -1,6 +1,7 @@
 import CardStyles from './Card.module.scss';
 import { FC } from 'react';
 import { Link } from 'react-router-dom';
+import { PathsContent } from '../../content';
 interface CardProps {
   title: string;
   image: string;
@@ -10,8 +11,10 @@ interface CardProps {
 }
 
 const Card: FC<CardProps> = ({ title, image, author, summary, id }) => {
+  const { podcast } = PathsContent;
+
   return (
-    <Link to={`/podcast/${id}`} state={{ summary }}>
+    <Link to={`${podcast} ${id}`} state={{ summary }}>
       <article className={CardStyles.container}>
         <img src={image} alt={title} />
         <h3>{title}</h3>
