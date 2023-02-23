5f874074558f1c3c0f8638fa50f8d9d2d5cf48f8
Oscar <ovenor11@proton.me>
2023-02-23 17:51:52 +0100

    feature - filter podcasts

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,13 +1,33 @@
-import { FC } from 'react';
+import { ChangeEvent, FC, useEffect, useState } from 'react';
+
 import { SearchContent } from '../../content';
 import SearchStyles from './Search.module.scss';
 
-const Search: FC = () => {
+interface SearchProps {
+  fn: any;
+}
+
+const Search: FC<SearchProps> = ({ fn }) => {
+  const [state, setState] = useState('');
   const { placeholder } = SearchContent;
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setState(e.target.value);
+  };
+
+  useEffect(() => {
+    fn(state);
+  }, [state]);
 
   return (
     <div>
-      <input type="text" placeholder={placeholder} className={SearchStyles.input} />
+      <input
+        type="text"
+        placeholder={placeholder}
+        className={SearchStyles.input}
+        value={state}
+        onChange={handleChange}
+      />
     </div>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,7 @@
+import { FC, useEffect, useState } from 'react';
+
 import Card from '../components/Podcast/Card';
-import { FC } from 'react';
+import { Entry } from '../../server/src/feed/feed-types';
 import HomeStyles from './styles/Home.module.scss';
 import Loader from '../components/common/Loader';
 import Search from '../components/search/Search';
@@ -7,23 +9,46 @@ import { fetchPodcasts } from '../utils';
 import { useQuery } from '@tanstack/react-query';
 
 const Home: FC = () => {
+  const [search, setSearch] = useState('');
+  const [list, setList] = useState<Entry[]>([]);
+
+  const getFilter = (filter: '') => {
+    setSearch(filter);
+  };
+
   const { isLoading, data } = useQuery(['podcasts'], fetchPodcasts);
+
+  useEffect(() => {
+    if (search !== '') {
+      setList(
+        data?.feed?.entry.filter(
+          (el: Entry) =>
+            el.title.label.toLowerCase().includes(search.toLowerCase()) ||
+            el['im:artist'].label.toLowerCase().includes(search.toLowerCase())
+        )
+      );
+    } else {
+      setList(data?.feed?.entry);
+    }
+  }, [data, search]);
 
   if (isLoading) return <Loader />;
 
   return (
     <div className={HomeStyles.container}>
-      <Search />
+      <Search fn={getFilter} />
       <div className={HomeStyles.cardList}>
-        {data.feed?.entry.map((el: any) => (
-          <Card
-            key={el.id.attributes['im:id']}
-            title={el.title.label}
-            author={el['im:artist'].label}
-            image={el['im:image'][2].label}
-            id={el.id.attributes['im:id']}
-          />
-        ))}
+        {list && list.length
+          ? list.map((el: Entry) => (
+              <Card
+                key={el.id.attributes['im:id']}
+                title={el.title.label}
+                author={el['im:artist'].label}
+                image={el['im:image'][2].label}
+                id={el.id.attributes['im:id']}
+              />
+            ))
+          : null}
       </div>
     </div>
   );
