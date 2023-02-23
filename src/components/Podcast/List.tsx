06f21e79b89579e3ef3054f6bd551b2b72868fd4
Oscar <ovenor11@proton.me>
2023-02-23 09:01:13 +0100

    fix - list element styles

--- src/components/Podcast/List.tsx
+++ src/components/Podcast/List.tsx
@@ -1,5 +1,6 @@
 import { EpisodesData } from '../../../server/src/feed/detail-types';
 import { FC } from 'react';
+import ListElement from './ListElement';
 import ListStyles from './List.module.scss';
 import { getDuration } from '../../utils';
 interface ListProps {
@@ -13,13 +14,16 @@ const List: FC<ListProps> = ({ count, episodes }) => {
   return (
     <div className={ListStyles.container}>
       <p>Episodes: {count}</p>
-      {episodes.map((el) => (
-        <div key={el.id}>
-          <h4>{el.attributes?.name}</h4>
-          <h5>{el.attributes?.releaseDateTime}</h5>
-          <h5>{getDuration(el.attributes?.durationInMilliseconds)}</h5>
-        </div>
-      ))}
+      <ul>
+        {episodes.map((el) => (
+          <ListElement
+            key={el.id}
+            name={el.attributes?.name}
+            date={el.attributes?.releaseDateTime}
+            duration={getDuration(el.attributes?.durationInMilliseconds)}
+          />
+        ))}
+      </ul>
     </div>
   );
 };
