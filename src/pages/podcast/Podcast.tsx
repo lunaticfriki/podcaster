1efa39e6864ed761210f8db574f0b46af0e32a1f
Oscar <ovenor11@proton.me>
2023-02-24 16:29:04 +0100

    fix - styles and sidebar link

--- src/pages/podcast/Podcast.tsx
+++ src/pages/podcast/Podcast.tsx
@@ -1,10 +1,10 @@
 import { Suspense, lazy } from 'react';
-import { useLocation, useParams } from 'react-router-dom';
 
 import { FC } from 'react';
 import Loader from '../../components/common/Loader';
 import PodcastStyles from './styles/Podcast.module.scss';
 import { fetchPodcast } from '../../utils';
+import { useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 
 const List = lazy(() => import('../../components/Podcast/List'));
@@ -13,12 +13,12 @@ const Sidebar = lazy(() => import('../../components/Sidebar/Sidebar'));
 const Podcast: FC = () => {
   const { id } = useParams();
   const { isLoading, data } = useQuery(['podcast', id], fetchPodcast);
-  const { state } = useLocation();
 
   const podcast = data?.mainPodcastInfo.feed.entry.find(
     (entry: any) => entry.id.attributes['im:id'] === id
   );
 
+  console.log(podcast?.id);
   if (isLoading && !data) {
     return <Loader />;
   } else {
@@ -29,8 +29,8 @@ const Podcast: FC = () => {
             title={data?.info.results[0].trackName}
             author={data?.info.results[0].artistName}
             cover={data?.info.results[0].artworkUrl600}
-            description={podcast.summary.label}
-            id={podcast.id}
+            description={podcast?.summary.label}
+            id={podcast?.id.attributes['im:id']}
           >
             <Suspense>
               <List count={data?.info.resultCount} episodes={data?.episodes} />
