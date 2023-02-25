75ca2184d9466cc2de87218142612e4cad9f1a17
Oscar <ovenor11@proton.me>
2023-02-25 13:25:51 +0100

    fix - error page

--- src/pages/podcast/Podcast.tsx
+++ src/pages/podcast/Podcast.tsx
@@ -1,5 +1,6 @@
 import { Suspense, lazy } from 'react';
 
+import ErrorPage from '../ErrorPage';
 import { FC } from 'react';
 import Loader from '../../components/common/Loader';
 import PodcastStyles from './Podcast.module.scss';
@@ -12,33 +13,36 @@ const Sidebar = lazy(() => import('../../components/Sidebar/Sidebar'));
 
 const Podcast: FC = () => {
   const { id } = useParams();
-  const { isLoading, data } = useQuery(['podcast', id], fetchPodcast);
+  const { isLoading, data, isError } = useQuery(['podcast', id], fetchPodcast);
 
   if (isLoading && !data) {
     return <Loader />;
-  } else {
-    const podcast = data?.mainPodcastInfo.feed.entry.find(
-      (entry: any) => entry.id.attributes['im:id'] === id
-    );
-
-    return (
-      <Suspense>
-        <div className={PodcastStyles.container}>
-          <Sidebar
-            title={data?.info.results[0].trackName}
-            author={data?.info.results[0].artistName}
-            cover={data?.info.results[0].artworkUrl600}
-            description={podcast?.summary.label}
-            id={podcast?.id.attributes['im:id']}
-          >
-            <Suspense>
-              <List count={data?.info.resultCount} episodes={data?.episodes} />
-            </Suspense>
-          </Sidebar>
-        </div>
-      </Suspense>
-    );
   }
+  const podcast = data?.mainPodcastInfo.feed.entry.find(
+    (entry: any) => entry.id.attributes['im:id'] === id
+  );
+
+  if (!data?.episodes.length) {
+    return <ErrorPage />;
+  }
+
+  return (
+    <Suspense>
+      <div className={PodcastStyles.container}>
+        <Sidebar
+          title={data?.info.results[0].trackName}
+          author={data?.info.results[0].artistName}
+          cover={data?.info.results[0].artworkUrl600}
+          description={podcast?.summary.label}
+          id={podcast?.id.attributes['im:id']}
+        >
+          <Suspense>
+            <List count={data?.info.resultCount} episodes={data?.episodes} />
+          </Suspense>
+        </Sidebar>
+      </div>
+    </Suspense>
+  );
 };
 
 export default Podcast;
--- src/pages/podcast/episode/Episode.tsx
+++ src/pages/podcast/episode/Episode.tsx
@@ -1,8 +1,7 @@
-import { Suspense, lazy, useEffect } from 'react';
+import { Suspense, lazy } from 'react';
 
 import Detail from '../../../components/Podcast/Detail';
 import EpisodeStyles from './Episode.module.scss';
-import { EpisodesData } from '../../../../server/src/feed/detail-types';
 import ErrorPage from '../../ErrorPage';
 import { FC } from 'react';
 import Loader from '../../../components/common/Loader';
@@ -18,35 +17,39 @@ const Episode: FC = () => {
 
   if (isLoading && !data) {
     return <Loader />;
-  } else {
-    const podcast = data?.mainPodcastInfo.feed.entry.find(
-      (entry: any) => entry.id.attributes['im:id'] === id
-    );
-
-    const episode = data?.episodes?.find((el: any) => el.trackId.toString() === episodeId);
-
-    return (
-      <Suspense>
-        <div className={EpisodeStyles.container}>
-          {true ? (
-            <Sidebar
-              title={data?.info.results[0].trackName}
-              author={data?.info.results[0].artistName}
-              cover={data?.info.results[0].artworkUrl600}
-              description={podcast?.summary.label}
-              id={podcast?.id.attributes['im:id']}
-            >
-              <Suspense>
-                <Detail episodeDetail={episode} />
-              </Suspense>
-            </Sidebar>
-          ) : (
-            <ErrorPage />
-          )}
-        </div>
-      </Suspense>
-    );
   }
+
+  if (!data?.episodes.length) {
+    return <ErrorPage />;
+  }
+
+  const podcast = data?.mainPodcastInfo.feed.entry.find(
+    (entry: any) => entry.id.attributes['im:id'] === id
+  );
+
+  const episode = data?.episodes?.find((el: any) => el.trackId.toString() === episodeId);
+
+  return (
+    <Suspense>
+      <div className={EpisodeStyles.container}>
+        {true ? (
+          <Sidebar
+            title={data?.info.results[0].trackName}
+            author={data?.info.results[0].artistName}
+            cover={data?.info.results[0].artworkUrl600}
+            description={podcast?.summary.label}
+            id={podcast?.id.attributes['im:id']}
+          >
+            <Suspense>
+              <Detail episodeDetail={episode} />
+            </Suspense>
+          </Sidebar>
+        ) : (
+          <ErrorPage />
+        )}
+      </div>
+    </Suspense>
+  );
 };
 
 export default Episode;
